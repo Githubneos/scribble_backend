893e4b2202ac2c633faf51017487d95090883a72
Im2008 <142449344+Im2008@users.noreply.github.com>
2024-12-12 10:22:02 -0800

    Update scribl.py

--- api/scribl.py
+++ api/scribl.py
@@ -1,6 +1,9 @@
+import jwt
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from __init__ import app
+from api.jwt_authorize import token_required
 
 app = Flask(__name__)
 
