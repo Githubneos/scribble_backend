b884d88d88b24ad65b22795df87469907cda171a
Githubneos <keerthansaikarumudi@gmail.com>
2025-01-16 10:15:05 -0800

    message

--- main.py
+++ main.py
@@ -45,12 +45,7 @@ from model.post import Post, initPosts
 from model.nestPost import NestPost, initNestPosts # Justin added this, custom format for his website
 from model.vote import Vote, initVotes
 from model.guess import Guess
-<<<<<<< HEAD
 from model.leaderboard import  initLeaderboardTable  # Import the LeaderboardEntry model and init function
-=======
-from model.leaderboard import LeaderboardEntry, initLeaderboardTable  # Import the LeaderboardEntry model and init function
-from model.competition import initTimerDataTable
->>>>>>> 8e50da7f2fc09c8559c4f45289cfd599306ed268
 # server only Views
 
 # register URIs for api endpoints
