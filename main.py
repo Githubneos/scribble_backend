910643d92512fa91ec332e488dc735cc57380280
im2008 <ianm02879@gmail.com>
2025-01-16 00:15:52 -0800

    initTimeDataTable -> user_manangement.db

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ from model.post import Post, initPosts
 from model.nestPost import NestPost, initNestPosts # Justin added this, custom format for his website
 from model.vote import Vote, initVotes
 from model.guess import Guess
-<<<<<<< HEAD
 from model.leaderboard import  initLeaderboardTable  # Import the LeaderboardEntry model and init function
+from model.competition import initTimerDataTable
 # server only Views
 
 # register URIs for api endpoints
