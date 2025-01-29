97d013a9c1e3fcb0b04121bdef04b97c070960c1
Githubneos <keerthansaikarumudi@gmail.com>
2025-01-29 11:07:05 -0800

    switching port for deployment

--- main.py
+++ main.py
@@ -719,4 +719,4 @@ def save_drawing():
 # this runs the flask application on the development server
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True, host="0.0.0.0", port="8887")
+    app.run(debug=True, host="0.0.0.0", port="8209")
